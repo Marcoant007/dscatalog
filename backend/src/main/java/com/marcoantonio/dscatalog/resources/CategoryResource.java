8f7d394bdb1b546c97744be08689e7bf9003ec64
Marco Antonio De Araujo Junior <59663846+Marcoant007@users.noreply.github.com>
2022-02-17 18:10:15 -0300

    Database seeding

--- backend/src/main/java/com/marcoantonio/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/marcoantonio/dscatalog/resources/CategoryResource.java
@@ -1,17 +1,12 @@
 package com.marcoantonio.dscatalog.resources;
-
-import java.util.ArrayList;
 import java.util.List;
-
 import com.marcoantonio.dscatalog.entities.Category;
 import com.marcoantonio.dscatalog.services.CategoryServices;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
--- backend/src/main/java/com/marcoantonio/dscatalog/services/CategoryServices.java
+++ backend/src/main/java/com/marcoantonio/dscatalog/services/CategoryServices.java
@@ -7,6 +7,7 @@ import com.marcoantonio.dscatalog.repositories.CategoryRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoryServices {
@@ -14,6 +15,7 @@ public class CategoryServices {
     @Autowired
     private CategoryRepository respository;
     
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
         return respository.findAll();
     }
