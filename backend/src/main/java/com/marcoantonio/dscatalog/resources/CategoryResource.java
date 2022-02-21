e68a548a4d08ba14d67531f120bf2a6d8edda2ac
Marco Antonio De Araujo Junior <59663846+Marcoant007@users.noreply.github.com>
2022-02-21 18:28:05 -0300

    .

--- backend/src/main/java/com/marcoantonio/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/marcoantonio/dscatalog/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import com.marcoantonio.dscatalog.dtos.CategoryDTO;
 import com.marcoantonio.dscatalog.services.CategoryServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,6 +47,13 @@ public class CategoryResource {
     public ResponseEntity<CategoryDTO> updated(@PathVariable Long id , @RequestBody CategoryDTO categoryDTO){
         categoryDTO = service.updatedCategory(id, categoryDTO);
         return ResponseEntity.ok().body(categoryDTO);
+    }
+
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> updated(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
