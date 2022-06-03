23db166dfd1d7af055a609a20e633a28fbb3d431
Marco Antonio De Araujo Junior <59663846+Marcoant007@users.noreply.github.com>
2022-06-03 16:08:35 -0300

    H2 security configuration

--- backend/src/main/java/com/marcoantonio/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/marcoantonio/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.marcoantonio.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -14,9 +17,12 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Autowired
+    private Environment env;
+
+    @Autowired
     private JwtTokenStore tokenStore;
 
-    private static final String[] ROUTES_PUBLIC = {"/oauth/token"};
+    private static final String[] ROUTES_PUBLIC = {"/oauth/token", "/h2-console/**"};
 
     private static final String[] ROUTES_OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
 
@@ -31,6 +37,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        //essa configuração é para liberar o h2
+        if(Arrays.asList(env.getActiveProfiles()).contains("test")){
+            http.headers().frameOptions().disable();
+        }
+
         http.authorizeRequests()
         .antMatchers(ROUTES_PUBLIC).permitAll() //quem tiver acessando o vetor de rota publica, permite acesso o
         .antMatchers(HttpMethod.GET, ROUTES_OPERATOR_OR_ADMIN).permitAll() // vou permitir so visualizar(get) quem for do Operator or admin
