c71eb96265e1391ff0ad16db996beea58c02e648
Marcoant007 <marcoantnovo@gmail.com>
2022-06-07 21:02:58 -0300

    Cors configuration

--- backend/src/main/java/com/marcoantonio/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/marcoantonio/dscatalog/config/ResourceServerConfig.java
@@ -3,7 +3,10 @@ package com.marcoantonio.dscatalog.config;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +14,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -22,33 +29,54 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Autowired
     private JwtTokenStore tokenStore;
 
-    private static final String[] ROUTES_PUBLIC = {"/oauth/token", "/h2-console/**"};
+    private static final String[] ROUTES_PUBLIC = { "/oauth/token", "/h2-console/**" };
 
-    private static final String[] ROUTES_OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
+    private static final String[] ROUTES_OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
 
-    private static final String[] ROUTES_ADMIN = {"/users/**"};
-
+    private static final String[] ROUTES_ADMIN = { "/users/**" };
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        //configurar token store
+        // configurar token store
         resources.tokenStore(tokenStore); // com isso o resource server vai decodificar o token e analisar se ta valido
     }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
 
-        //essa configuração é para liberar o h2
-        if(Arrays.asList(env.getActiveProfiles()).contains("test")){
+        // essa configuração é para liberar o h2
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
             http.headers().frameOptions().disable();
         }
 
         http.authorizeRequests()
-        .antMatchers(ROUTES_PUBLIC).permitAll() //quem tiver acessando o vetor de rota publica, permite acesso o
-        .antMatchers(HttpMethod.GET, ROUTES_OPERATOR_OR_ADMIN).permitAll() // vou permitir so visualizar(get) quem for do Operator or admin
-        .antMatchers(ROUTES_OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN") //só acessa se possuir algum desses atributos
-        .antMatchers(ROUTES_ADMIN).hasRole("ADMIN")// só acessa essa rota se for admin
-        .anyRequest().authenticated(); // se acessar qualquer outra rota além dessa, só se estiver autenticado
+            .antMatchers(ROUTES_PUBLIC).permitAll() // quem tiver acessando o vetor de rota publica, permite acesso 
+            .antMatchers(HttpMethod.GET, ROUTES_OPERATOR_OR_ADMIN).permitAll() // vou permitir so visualizar(get) quem for do Operator or admin
+            .antMatchers(ROUTES_OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN") // só acessa se possuir algum desses atributos
+            .antMatchers(ROUTES_ADMIN).hasRole("ADMIN")// só acessa essa rota se for admin
+            .anyRequest().authenticated(); // se acessar qualquer outra rota além dessa, só se estiver autenticado
+            http.cors().configurationSource(corsConfigurationSource());
     }
-    
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOriginPatterns(Arrays.asList("*"));
+        corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(
+                new CorsFilter(corsConfigurationSource()));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+
 }
