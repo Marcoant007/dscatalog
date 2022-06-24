a95abf1aa435c2fb28295cc906e40866e381809f
Marco Antonio De Araujo Junior <59663846+Marcoant007@users.noreply.github.com>
2022-06-24 14:23:12 -0300

    Fix: Postgres compatibility

--- backend/src/main/java/com/marcoantonio/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/marcoantonio/dscatalog/repositories/ProductRepository.java
@@ -3,6 +3,8 @@ package com.marcoantonio.dscatalog.repositories;
 import com.marcoantonio.dscatalog.entities.Category;
 import com.marcoantonio.dscatalog.entities.Product;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>{
-    @Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories categorias WHERE (:category IS NULL OR :category IN categorias)")
-    Page<Product> find(Category category, Pageable pageble);
+    @Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories categorias WHERE (COALESCE(:categories) IS NULL OR categorias IN :categories)"
+    + "AND (LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%')))")
+    Page<Product> find(List<Category> categories, String name, Pageable pageble);
 }
--- backend/src/main/java/com/marcoantonio/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/marcoantonio/dscatalog/resources/ProductResource.java
@@ -33,12 +33,13 @@ public class ProductResource {
     @GetMapping
     public ResponseEntity<Page<ProductDTO>> findAll(
             @RequestParam(value = "categoryId", defaultValue = "0")Long categoryId,
+            @RequestParam(value = "name", defaultValue = "") String name,
             @RequestParam(value = "page", defaultValue = "0") Integer page,
             @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
             @RequestParam(value = "direction", defaultValue = "ASC") String direction,
             @RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
          PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-         Page<ProductDTO> list = service.findAllPaged(categoryId, pageRequest);
+         Page<ProductDTO> list = service.findAllPaged(categoryId, name.trim(), pageRequest);
          return ResponseEntity.ok().body(list);   
     }
 
